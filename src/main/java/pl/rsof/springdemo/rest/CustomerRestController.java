47984c43a006f4b794cbb2d4dccbb12fa5b91d93
Robert Siwak <robert.siwak@autosan.pl>
2022-01-26 14:24:49 +0100

    spring-crm-rest: CRUD method

--- src/main/java/pl/rsof/springdemo/rest/CustomerRestController.java
+++ src/main/java/pl/rsof/springdemo/rest/CustomerRestController.java
@@ -10,8 +10,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,5 +53,43 @@ public class CustomerRestController {
 			throw new CustomerNotFoundException("Customer not found, id: " + customerId);
 		}
 	}
+	
+	
+	// mapping for POST / customers - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		
+		// force to add (insert) customer - null or zero for DAO
+		customer.setId(0);
+		
+		customerService.saveCustomer(customer);
+		
+		return customer;
+	}
+	
+	
+	// mapping for POST / customers - update existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+
+		customerService.saveCustomer(customer);
+
+		return customer;
+	}
+	
+	
+	// mapping for DELETE / customers/{customerId} - delete customer
+	@DeleteMapping("customer/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		Customer customer = customerService.getCustomer(customerId);
+		if (customer == null) {
+			throw new CustomerNotFoundException("Customer not found, id: " + customerId);
+		}
+		
+		customerService.deleteCustomer(customerId);
+		
+		return "Deleted customer, id: "+customerId;
+	}
+	
 	
 }
